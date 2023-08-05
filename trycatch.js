5e8d2f9e1f959845ee6912b231f89c14b889d86e
Nigam Sharma <132887723+THARMATT@users.noreply.github.com>
2023-08-05 14:28:04 +0530

    Create trycatch.js

---
+++ trycatch.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+  console.log("lets hack someone wifi")
+},4000)
+try{
+  console.log(rahul)
+}catch(error){
+  console.log(error )
+}
+
+setTimeout(()=>{
+  console.log("lets hack duffls wifi")
+},5000)
+setTimeout(()=>{
+  console.log("hacked...")
+},6000)
